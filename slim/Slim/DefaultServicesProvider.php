d6bf1840f995b246e213d6456a8e5275cb108006
plding <77676182@qq.com>
2017-06-29 14:07:36 +0800

    update

---
+++ slim/Slim/DefaultServicesProvider.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2011-2017 Josh Lockhart
+ * @license   https://github.com/slimphp/Slim/blob/3.x/LICENSE.md (MIT License)
+ */
+namespace Slim;
+
+use Slim\Http\Environment;
+
+/**
+ * Slim's default Service Provider.
+ */
+class DefaultServicesProvider
+{
+    /**
+     * Register Slim's default services.
+     *
+     * @param Container $container A DI container implementing ArrayAccess and container-interop.
+     */
+    public function register($container)
+    {
+        if (!isset($container['environment'])) {
+            /**
+             * This service MUST return a shared instance
+             * of \Slim\Interfaces\Http\EnvironmentInterface.
+             *
+             * @return EnvironmentInterface
+             */
+            $container['environment'] = function () {
+                return new Environment($_SERVER);
+            };
+        }
+    }
+}
