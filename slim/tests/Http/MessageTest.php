46dacd5a7cc6629d2d061bc3f635af27ca1e202c
plding <77676182@qq.com>
2017-06-29 15:43:27 +0800

    update

--- slim/tests/Http/MessageTest.php
+++ slim/tests/Http/MessageTest.php
@@ -28,186 +28,186 @@ class MessageTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('1.0', $message->getProtocolVersion());
     }
 
-    // /**
-    //  * @covers Slim\Http\Message::withProtocolVersion
-    //  */
-    // public function testWithProtocolVersion()
-    // {
-    //     $message = new MessageStub();
-    //     $clone = $message->withProtocolVersion('1.0');
+    /**
+     * @covers Slim\Http\Message::withProtocolVersion
+     */
+    public function testWithProtocolVersion()
+    {
+        $message = new MessageStub();
+        $clone = $message->withProtocolVersion('1.0');
 
-    //     $this->assertEquals('1.0', $clone->protocolVersion);
-    // }
+        $this->assertEquals('1.0', $clone->protocolVersion);
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::withProtocolVersion
-    //  * @expectedException \InvalidArgumentException
-    //  */
-    // public function testWithProtocolVersionInvalidThrowsException()
-    // {
-    //     $message = new MessageStub();
-    //     $message->withProtocolVersion('3.0');
-    // }
+    /**
+     * @covers Slim\Http\Message::withProtocolVersion
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWithProtocolVersionInvalidThrowsException()
+    {
+        $message = new MessageStub();
+        $message->withProtocolVersion('3.0');
+    }
 
-    // /*******************************************************************************
-    //  * Headers
-    //  ******************************************************************************/
+    /*******************************************************************************
+     * Headers
+     ******************************************************************************/
 
-    // /**
-    //  * @covers Slim\Http\Message::getHeaders
-    //  */
-    // public function testGetHeaders()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
-    //     $headers->add('X-Foo', 'two');
-    //     $headers->add('X-Foo', 'three');
+    /**
+     * @covers Slim\Http\Message::getHeaders
+     */
+    public function testGetHeaders()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
+        $headers->add('X-Foo', 'two');
+        $headers->add('X-Foo', 'three');
 
-    //     $message = new MessageStub();
-    //     $message->headers = $headers;
+        $message = new MessageStub();
+        $message->headers = $headers;
 
-    //     $shouldBe = [
-    //         'X-Foo' => [
-    //             'one',
-    //             'two',
-    //             'three',
-    //         ],
-    //     ];
+        $shouldBe = [
+            'X-Foo' => [
+                'one',
+                'two',
+                'three',
+            ],
+        ];
 
-    //     $this->assertEquals($shouldBe, $message->getHeaders());
-    // }
+        $this->assertEquals($shouldBe, $message->getHeaders());
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::hasHeader
-    //  */
-    // public function testHasHeader()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
+    /**
+     * @covers Slim\Http\Message::hasHeader
+     */
+    public function testHasHeader()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
 
-    //     $message = new MessageStub();
-    //     $message->headers = $headers;
+        $message = new MessageStub();
+        $message->headers = $headers;
 
-    //     $this->assertTrue($message->hasHeader('X-Foo'));
-    //     $this->assertFalse($message->hasHeader('X-Bar'));
-    // }
+        $this->assertTrue($message->hasHeader('X-Foo'));
+        $this->assertFalse($message->hasHeader('X-Bar'));
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::getHeaderLine
-    //  */
-    // public function testGetHeaderLine()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
-    //     $headers->add('X-Foo', 'two');
-    //     $headers->add('X-Foo', 'three');
+    /**
+     * @covers Slim\Http\Message::getHeaderLine
+     */
+    public function testGetHeaderLine()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
+        $headers->add('X-Foo', 'two');
+        $headers->add('X-Foo', 'three');
 
-    //     $message = new MessageStub();
-    //     $message->headers = $headers;
+        $message = new MessageStub();
+        $message->headers = $headers;
 
-    //     $this->assertEquals('one,two,three', $message->getHeaderLine('X-Foo'));
-    //     $this->assertEquals('', $message->getHeaderLine('X-Bar'));
-    // }
+        $this->assertEquals('one,two,three', $message->getHeaderLine('X-Foo'));
+        $this->assertEquals('', $message->getHeaderLine('X-Bar'));
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::getHeader
-    //  */
-    // public function testGetHeader()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
-    //     $headers->add('X-Foo', 'two');
-    //     $headers->add('X-Foo', 'three');
+    /**
+     * @covers Slim\Http\Message::getHeader
+     */
+    public function testGetHeader()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
+        $headers->add('X-Foo', 'two');
+        $headers->add('X-Foo', 'three');
 
-    //     $message = new MessageStub();
-    //     $message->headers = $headers;
+        $message = new MessageStub();
+        $message->headers = $headers;
 
-    //     $this->assertEquals(['one', 'two', 'three'], $message->getHeader('X-Foo'));
-    //     $this->assertEquals([], $message->getHeader('X-Bar'));
-    // }
+        $this->assertEquals(['one', 'two', 'three'], $message->getHeader('X-Foo'));
+        $this->assertEquals([], $message->getHeader('X-Bar'));
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::withHeader
-    //  */
-    // public function testWithHeader()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
-    //     $message = new MessageStub();
-    //     $message->headers = $headers;
-    //     $clone = $message->withHeader('X-Foo', 'bar');
+    /**
+     * @covers Slim\Http\Message::withHeader
+     */
+    public function testWithHeader()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
+        $message = new MessageStub();
+        $message->headers = $headers;
+        $clone = $message->withHeader('X-Foo', 'bar');
 
-    //     $this->assertEquals('bar', $clone->getHeaderLine('X-Foo'));
-    // }
+        $this->assertEquals('bar', $clone->getHeaderLine('X-Foo'));
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::withAddedHeader
-    //  */
-    // public function testWithAddedHeader()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
-    //     $message = new MessageStub();
-    //     $message->headers = $headers;
-    //     $clone = $message->withAddedHeader('X-Foo', 'two');
+    /**
+     * @covers Slim\Http\Message::withAddedHeader
+     */
+    public function testWithAddedHeader()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
+        $message = new MessageStub();
+        $message->headers = $headers;
+        $clone = $message->withAddedHeader('X-Foo', 'two');
 
-    //     $this->assertEquals('one,two', $clone->getHeaderLine('X-Foo'));
-    // }
+        $this->assertEquals('one,two', $clone->getHeaderLine('X-Foo'));
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::withoutHeader
-    //  */
-    // public function testWithoutHeader()
-    // {
-    //     $headers = new Headers();
-    //     $headers->add('X-Foo', 'one');
-    //     $headers->add('X-Bar', 'two');
-    //     $response = new MessageStub();
-    //     $response->headers = $headers;
-    //     $clone = $response->withoutHeader('X-Foo');
-    //     $shouldBe = [
-    //         'X-Bar' => ['two'],
-    //     ];
+    /**
+     * @covers Slim\Http\Message::withoutHeader
+     */
+    public function testWithoutHeader()
+    {
+        $headers = new Headers();
+        $headers->add('X-Foo', 'one');
+        $headers->add('X-Bar', 'two');
+        $response = new MessageStub();
+        $response->headers = $headers;
+        $clone = $response->withoutHeader('X-Foo');
+        $shouldBe = [
+            'X-Bar' => ['two'],
+        ];
 
-    //     $this->assertEquals($shouldBe, $clone->getHeaders());
-    // }
+        $this->assertEquals($shouldBe, $clone->getHeaders());
+    }
 
-    // /*******************************************************************************
-    //  * Body
-    //  ******************************************************************************/
+    /*******************************************************************************
+     * Body
+     ******************************************************************************/
 
-    // /**
-    //  * @covers Slim\Http\Message::getBody
-    //  */
-    // public function testGetBody()
-    // {
-    //     $body = $this->getBody();
-    //     $message = new MessageStub();
-    //     $message->body = $body;
+    /**
+     * @covers Slim\Http\Message::getBody
+     */
+    public function testGetBody()
+    {
+        $body = $this->getBody();
+        $message = new MessageStub();
+        $message->body = $body;
 
-    //     $this->assertSame($body, $message->getBody());
-    // }
+        $this->assertSame($body, $message->getBody());
+    }
 
-    // /**
-    //  * @covers Slim\Http\Message::withBody
-    //  */
-    // public function testWithBody()
-    // {
-    //     $body = $this->getBody();
-    //     $body2 = $this->getBody();
-    //     $message = new MessageStub();
-    //     $message->body = $body;
-    //     $clone = $message->withBody($body2);
+    /**
+     * @covers Slim\Http\Message::withBody
+     */
+    public function testWithBody()
+    {
+        $body = $this->getBody();
+        $body2 = $this->getBody();
+        $message = new MessageStub();
+        $message->body = $body;
+        $clone = $message->withBody($body2);
 
-    //     $this->assertSame($body, $message->body);
-    //     $this->assertSame($body2, $clone->body);
-    // }
+        $this->assertSame($body, $message->body);
+        $this->assertSame($body2, $clone->body);
+    }
 
-    // /**
-    //  * @return \PHPUnit_Framework_MockObject_MockObject|\Slim\Http\Body
-    //  */
-    // protected function getBody()
-    // {
-    //     return $this->getMockBuilder('Slim\Http\Body')->disableOriginalConstructor()->getMock();
-    // }
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Slim\Http\Body
+     */
+    protected function getBody()
+    {
+        return $this->getMockBuilder('Slim\Http\Body')->disableOriginalConstructor()->getMock();
+    }
 }
